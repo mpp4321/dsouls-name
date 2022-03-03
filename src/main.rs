7adfe3888ece9ec2bc040e4f6378ecf4bc9c27f0
mpp4321 <47093830+mpp4321@users.noreply.github.com>
2022-03-02 19:24:57 -0500

    Initial

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ lazy_static! {
     static ref ADJECTIVES: Vec<String> = read_lines_vec("adjectives.txt");
     static ref PLACES: Vec<String> = read_lines_vec("places.txt");
     static ref SUFFIXES: Vec<String> = read_lines_vec("suffixes.txt");
+    static ref TITLES: Vec<String> = read_lines_vec("titles.txt");
 }
 
 const DECKED: bool = true;
@@ -27,19 +28,32 @@ fn pick_from<T>(items: &[T]) -> &T {
     return &items[random_len];
 }
 
+fn do_title(choose_title: bool, titles_ref: &[String], working_name: &mut Builder) {
+    if choose_title {
+        let a = pick_from(titles_ref).clone();
+        working_name.append(", ");
+        working_name.append(a.as_bytes());
+    }
+}
+
 fn generate_random_name() -> String {
     let mut working_name: Builder = Builder::default();
 
     // An adjective prefix
     let choose_prefix = rand::random::<bool>() || DECKED;
 
+
     // A word from the suffix list
     let choose_suffix = rand::random::<bool>() || DECKED;
+
+    // Title
+    let choose_title = (rand::random::<bool>() || DECKED) && choose_suffix;
     
     let nouns_ref: &[String] = &*NOUNS;
     let adjectives_ref: &[String] = &*ADJECTIVES;
     let places_ref: &[String] = &*PLACES;
     let suffixes_ref: &[String] = &*SUFFIXES;
+    let titles_ref: &[String] = &*TITLES;
 
     let noun = pick_from(nouns_ref);
     if choose_prefix {
@@ -49,20 +63,28 @@ fn generate_random_name() -> String {
     }
     working_name.append(noun.as_bytes());
     if choose_suffix {
-        match rand::random::<i8>() % 3 {
+        match rand::random::<i8>() % 4 {
             0 => {
+                do_title(choose_title, titles_ref, &mut working_name);
                 let a = pick_from(nouns_ref).clone();
-                working_name.append(" slayer of the ".as_bytes());
-                working_name.append(a.as_bytes());
-            },
-            1 => {
-                let a = pick_from(places_ref).clone();
                 working_name.append(" of the ".as_bytes());
                 working_name.append(a.as_bytes());
             },
+            1 => {
+                do_title(choose_title, titles_ref, &mut working_name);
+                let a = pick_from(places_ref).clone();
+                working_name.append(" of ".as_bytes());
+                working_name.append(a.as_bytes());
+            },
             2 => {
+                do_title(choose_title, titles_ref, &mut working_name);
                 let a = pick_from(suffixes_ref).clone();
                 working_name.append(" of ".as_bytes());
+                working_name.append(a.as_bytes());
+            },
+            3 => {
+                let a = pick_from(nouns_ref).clone();
+                working_name.append(" ".as_bytes());
                 working_name.append(a.as_bytes());
             },
             _ => {},
@@ -81,7 +103,7 @@ fn do_name_with_sentence(lines: &Vec<Result<String, io::Error>>) {
 fn main() {
     let file_with_names = File::open("names.txt").expect("Did not find names.txt");
     let lines = io::BufReader::new(file_with_names).lines().collect::<Vec<Result<String, _>>>();
-    do_name_with_sentence(&lines);
-    do_name_with_sentence(&lines);
-    do_name_with_sentence(&lines);
+    for _ in 0..10 {
+        do_name_with_sentence(&lines);
+    }
 }
